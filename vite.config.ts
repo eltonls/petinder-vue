bb2eddd295a456daa775a68192abc57ba8e61b19
Hélen Andrade <50506811+HelenAndrade@users.noreply.github.com>
2024-10-25 16:06:29 -0300

    (feature)[Frontend] add proxy reverse (#5)

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const _API_URL_USER = `${env.VITE_URL_USER ?? ""}`;
+  const _API_URL_PET = `${env.VITE_URL_PET ?? ""}`;
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+
+    server: {
+      proxy: {
+        "/api/user": {
+          target: _API_URL_USER,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/api/pet": {
+          target: _API_URL_PET,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
