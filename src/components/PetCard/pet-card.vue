525d797627ca2d9691a01184f284050f5e4adecf
Victor Manoel Magalhães Timbó <68547537+VictorManoel-Timbo@users.noreply.github.com>
2024-10-29 13:08:48 -0300

    feature: integration card in home view (#11)

--- src/components/PetCard/pet-card.vue
+++ src/components/PetCard/pet-card.vue
@@ -1,24 +1,24 @@
 <template>
     <main class="m-4">
-        <Card class="min-w-72 w-[18vw] max-w-80 overflow-hidden rounded border-2">
+        <Card class="min-w-60 w-[20vw] max-w-80 overflow-hidden rounded border-2">
             <template #header>
                 <img :src="pet.imageCat" alt="pet image">
             </template>
             <template #title>
-                <span class="!font-bold line-clamp-1">{{ pet.name }}</span>
+                <span class="!font-bold line-clamp-1 text-start">{{ pet.name }}</span>
             </template>
             <template #subtitle>
                 <div class="gap-2 flex max-h-8">
-                    <Tag class="!bg-blue-600 max-w-34 !text-white !line-clamp-1" :value="pet.breed" rounded />
-                    <Tag class="!bg-blue-600 !text-white !truncate" :value="pet.gender" rounded />
-                    <Tag class="!bg-blue-600 !text-white !truncate" :value="ageStatus" rounded />
+                    <Tag class="!bg-blue-600 max-w-34 !text-white !line-clamp-1 px-1" :value="pet.breed" rounded />
+                    <Tag class="!bg-blue-600 !text-white !truncate px-1" :value="pet.gender" rounded />
+                    <Tag class="!bg-blue-600 !text-white !truncate px-1" :value="ageStatus" rounded />
                 </div>
             </template>
             <template #content>
-                <p class="text-justify my-2 truncate">{{ pet.description }}</p>
+                <p class="hidden md:flex text-justify my-2 truncate">{{ pet.description }}</p>
             </template>
             <template #footer>
-                <Button label="Quero adotar" class="w-full !bg-[#FF8C00] hover:!bg-white hover:!text-[#FF8C00] !border-[#FF8C00] active:scale-95 duration-200" />
+                <Button label="Quero adotar" class="w-full !bg-orange-400 hover:!bg-white hover:!text-orange-400 !border-orange-400 active:scale-95 duration-200" />
             </template>
         </Card>
     </main>
