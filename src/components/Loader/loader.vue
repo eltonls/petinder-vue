4e35f520922fe395f17d89ebc4afeb4710b8a8d5
Victor Manoel Magalhães Timbó <68547537+VictorManoel-Timbo@users.noreply.github.com>
2024-10-29 16:48:43 -0300

    feature: add loader in home (#14)

---
+++ src/components/Loader/loader.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center h-screen select-none">
+        <ProgressSpinner class="!h-32 !w-32" />
+    </div>
+</template>
