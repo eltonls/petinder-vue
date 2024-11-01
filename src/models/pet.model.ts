5c7e49a22c70af6208593b2e59654b7d6bde1a2c
Helen Andrade <helenaraujoandrade@hotmail.com>
2024-11-01 14:23:43 -0300

    (feature)[Frontend] add new rest and service for pets

--- src/models/pet.model.ts
+++ src/models/pet.model.ts
@@ -1,11 +1,38 @@
 export default class Pet{
     constructor(
         public name: string = '',
-        public breed: string = '',
+        public breed: Breed,
         public age: number,
         public description: string ,
-        public gender: string,
-        public imageCat: string,
-        public id: string
+        public gender: GenderPet,
+        public image_url: string,
+        public created_at: Date = new Date,
+        public id?: number,
+        public owner_id?: string
     ){}
+}
+
+export enum GenderPet {
+    Female = "Fêmea",
+    Male = "Macho"
+}
+
+export enum AgeRange {
+    Young = "Jovem",
+    Adult = "Adulto",
+    Old = "Idoso"
+}
+
+export enum Breed {
+    Dogs = "Cães",
+    Cats = "Gatos",
+    Rodents = "Roedores e coelhos", 
+    Birds = "Pássaros",
+    Lizards = "Lagartos",
+    Turtles = "Tartarugas",
+    Frogs = "Sapos e rãs",
+    Fish = "Peixes",
+    Insects = "Insetos",
+    Arachnids = "Aracnídeos",
+    Others = "Outros"
 }
--- src/views/Home/home.service.ts
+++ src/views/Home/home.service.ts
@@ -1,19 +1,57 @@
-//import { PetRest } from "@/services/rest/pet.rest";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import type { AgeRange, Breed, GenderPet } from "@/models/pet.model";
+import type Pet from "@/models/pet.model";
+import PetRest from "@/services/rest/pet.rest";
+import { Observable, Subject, take } from "rxjs";
 
 export class HomeService {
-  constructor(
-    //private _petRest = new PetRest()
-  ) { }
+  constructor(private _petRest = new PetRest()) {};
+  
   private pets$: Subject<any> = new Subject<any>();
+  
+  pets: Observable<any> = this.pets$.asObservable();
+  
+  
+  getPets(gender?: GenderPet, ageRange?: AgeRange, breed?: Breed): void {
+    this._petRest
+    .getPets(gender, ageRange, breed)
+    .pipe(take(1))
+    .subscribe({
+      next: (response) => {
+        this.pets$.next(response)
+      }
+    })
+  }
 
-  pets: Observable<any> = this.pets$.asObservable()
+  addPet(pet: Pet): void {
+    this._petRest
+    .addPet(pet)
+    .pipe(take(1))
+    .subscribe({
+      next: (response) => {
+        this.pets$.next(response)
+      }
+    })
+  }
 
-  // getAllPets(): void {
-  //   this._petRest.getAllPets().pipe().subscribe({
-  //     next: (response) => {
-  //       this.pets$.next(response)
-  //     }
-  //   })
-  // }
+  updatePet(pet: Pet): void {
+    this._petRest
+    .updatePet(pet)
+    .pipe(take(1))
+    .subscribe({
+      next: (response) => {
+        this.pets$.next(response)
+      }
+    })
+  }
+
+  deletePet(id: number): void {
+    this._petRest
+    .deletePet(id)
+    .pipe(take(1))
+    .subscribe({
+      next: (response) => {
+        this.pets$.next(response)
+      }
+    })
+  }
 }
